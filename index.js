8dba6774891fbc332cb81fd49a16a908b5fc7c2d
Cannicide <41762964+Cannicide@users.noreply.github.com>
2021-03-02 23:16:37 -0500

    Added main module file.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-//Module has not been developed yet. This is only a template at the moment.
+//Module has not been fully developed yet. This is only a template at the moment.
+//Requires testing before full use is authorized.
+
+const Handler = require('./handler');
+const Command = require("./command");
+const Interface = require("./interface");
+const EvG = require("./evg");
+const Interpreter = require("./interpreter");
+const Settings = require("./settings");
+
+class NodeElisif extends Handler {
+
+    Command = Command;
+    interface = Interface;
+    evg = EvG;
+    interpreter = Interpreter;
+    settings = Settings;
+
+    #instance;
+
+    constructor() {
+        this.#instance = this;
+    }
+
+    getInstance() {
+        return this;
+    }
+
+    getInstanceAtInitialize() {
+        return this.#instance;
+    }
+
+    getStatic() {
+        return NodeElisif;
+    }
+
+}
+
+module.exports = new NodeElisif();
