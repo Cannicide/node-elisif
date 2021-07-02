1f6c18f151deee2cee43d0400461cc2a03f5b402
Cannicide <41762964+Cannicide@users.noreply.github.com>
2021-07-02 19:04:10 -0400

    Added support for voice channel activities! (Early beta discord feature).

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# node-elisif [INCOMPLETE]
+# node-elisif
 A powerful Discord Bot container, command handler, and partial extension of discord.js designed to be both simplistic and feature-rich. Based on the command handler and various systems I developed for my three discord bots: Elisif, Panacea, and Scavenger.
 
-**Note: node-elisif has not been completed yet and is under construction.** Attempting to use it now may cause errors, and will not yield the expected results. Please return after the code has been developed to a usable extent.
+**Note: node-elisif has not been fully completed and tested yet.** Certain parts of the code, especially new features such as buttons and select menus, *may* cause errors.
+
+Several new updates are coming to node-elisif! Support for buttons and select menus has been added (still being worked on). Support for Voice Channel Activities (VCA) has been added; note, however, that this Discord feature is limited as it is still in early beta. A moderation expansion is also being worked on, and will be updated to include new buttons, alerts, punishment methods, auto-moderation, and more!
+
+No documentation currently exists for this module. Documentation is being worked on and will be linked here once completed. The upcoming moderation features and documentation will be available in node-elisif 2.x.x versions.
---
+++ vc_activities.js
@@ -0,0 +1,69 @@
+const fetch = require('node-fetch');
+
+const applications = {
+    'youtube':   '755600276941176913',
+    'poker':     '755827207812677713',
+    'betrayal':  '773336526917861400',
+    'fishing':   '814288819477020702',
+    'chess':     '832012586023256104'
+};
+
+class VCActivity {
+    
+    constructor(message, option) {
+        this.client = message.client;
+        this.message = message;
+        this.applications = applications;
+        this.option = option;
+    }
+
+    async _createCode() {
+      
+        var option = this.option;
+      
+        if (!this.message.member.voice.channel) return false;
+      
+        var voiceChannelId = this.message.member.voice.channelID;
+        let code = "none";
+      
+        if (option && option.toLowerCase() in this.applications) {
+          
+            let applicationID = this.applications[option.toLowerCase()];
+              
+            var res = await fetch(`https://discord.com/api/v8/channels/${voiceChannelId}/invites`, {
+                method: 'POST',
+                body: JSON.stringify({
+                    max_age: 86400,
+                    max_uses: 0,
+                    target_application_id: applicationID,
+                    target_type: 2,
+                    temporary: false,
+                    validate: null
+                }),
+                headers: {
+                    'Authorization': `Bot ${this.client.token}`,
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            var invite = await res.json();
+
+            if (invite.error || !invite.code) {
+                throw new Error('An error occurred while retrieving invite data...');
+            };
+
+            code = `https://discord.com/invite/${invite.code}`
+          
+            return code;
+          
+        }
+      
+        return false;
+    }
+  
+    get code() {
+      return this._createCode();
+    }
+}
+
+module.exports = VCActivity;
